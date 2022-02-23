ad2b0cc1aa1ee2199428d42ca3bf50f9be763623
senthamizharasur <senthamizharasur@MAVCHN1121153.mavericsystems.lan>
2022-02-23 16:27:26 +0530

    removed the getter setter

--- src/main/java/com/paywallet/userservice/user/model/LenderConfigInfo.java
+++ src/main/java/com/paywallet/userservice/user/model/LenderConfigInfo.java
@@ -18,53 +18,5 @@ public class LenderConfigInfo {
     private StateStatus validateSalaryAccountOwnership;
     private StateStatus validateAffordabilityCheck;
     private StateStatus invokeAndPublishDepositAllocation;
-	public String getLenderId() {
-		return lenderId;
-	}
-	public void setLenderId(String lenderId) {
-		this.lenderId = lenderId;
-	}
-	public String getLenderName() {
-		return lenderName;
-	}
-	public void setLenderName(String lenderName) {
-		this.lenderName = lenderName;
-	}
-	public StateStatus getPublishIdentityInfo() {
-		return publishIdentityInfo;
-	}
-	public void setPublishIdentityInfo(StateStatus publishIdentityInfo) {
-		this.publishIdentityInfo = publishIdentityInfo;
-	}
-	public StateStatus getPublishEmploymentInfo() {
-		return publishEmploymentInfo;
-	}
-	public void setPublishEmploymentInfo(StateStatus publishEmploymentInfo) {
-		this.publishEmploymentInfo = publishEmploymentInfo;
-	}
-	public StateStatus getPublishIncomeInfo() {
-		return publishIncomeInfo;
-	}
-	public void setPublishIncomeInfo(StateStatus publishIncomeInfo) {
-		this.publishIncomeInfo = publishIncomeInfo;
-	}
-	public StateStatus getValidateSalaryAccountOwnership() {
-		return validateSalaryAccountOwnership;
-	}
-	public void setValidateSalaryAccountOwnership(StateStatus validateSalaryAccountOwnership) {
-		this.validateSalaryAccountOwnership = validateSalaryAccountOwnership;
-	}
-	public StateStatus getValidateAffordabilityCheck() {
-		return validateAffordabilityCheck;
-	}
-	public void setValidateAffordabilityCheck(StateStatus validateAffordabilityCheck) {
-		this.validateAffordabilityCheck = validateAffordabilityCheck;
-	}
-	public StateStatus getInvokeAndPublishDepositAllocation() {
-		return invokeAndPublishDepositAllocation;
-	}
-	public void setInvokeAndPublishDepositAllocation(StateStatus invokeAndPublishDepositAllocation) {
-		this.invokeAndPublishDepositAllocation = invokeAndPublishDepositAllocation;
-	}
 
 }
