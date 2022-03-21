e9a6bc36349be0ad7c72052ebce9ab763038987e
Bhashyam B V <bhashyamb@maveric-systems.com>
2022-03-21 08:31:59 -0700

    Adding NotNull validations for the email, mobile and employer fields

--- src/main/java/com/paywallet/userservice/user/model/wrapperAPI/IdentityVerificationRequestWrapperModel.java
+++ src/main/java/com/paywallet/userservice/user/model/wrapperAPI/IdentityVerificationRequestWrapperModel.java
@@ -1,17 +1,21 @@
 package com.paywallet.userservice.user.model.wrapperAPI;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class IdentityVerificationRequestWrapperModel {
-	
+	@NotNull
 	private String employerId;
 	private String identityCallbackUrl;
 	private String firstName;
     private String lastName;
     private String addressLine1;
     private String addressLine2;
+    @NotNull
     private String emailId;
+    @NotNull
     private String cellPhone;
     private String dateOfBirth;
     private String last4TIN;
--- src/main/java/com/paywallet/userservice/user/model/wrapperAPI/IncomeVerificationRequestWrapperModel.java
+++ src/main/java/com/paywallet/userservice/user/model/wrapperAPI/IncomeVerificationRequestWrapperModel.java
@@ -1,12 +1,16 @@
 package com.paywallet.userservice.user.model.wrapperAPI;
 
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
 public class IncomeVerificationRequestWrapperModel {
-	
+	@NotNull
 	private String employerId;
+	@NotNull
 	private String emailId;
+	@NotNull
 	private String cellPhone;
 	private String incomeCallbackUrl;
 	private String firstName;
